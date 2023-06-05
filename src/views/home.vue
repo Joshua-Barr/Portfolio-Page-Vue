ac6b26f693953d4711cc5ec415b2d356509170f9
Joshua-Barr <joshbarrscience@gmail.com>
2023-06-05 09:52:50 +0800

    Commit from TeleportHQ

--- src/views/home.vue
+++ src/views/home.vue
@@ -2,29 +2,50 @@
   <div class="home-container">
     <div class="home-navbar">
       <span class="home-heading">Joshua Barr</span>
-      <svg viewBox="0 0 1024 1024" class="home-icon">
-        <path
-          d="M854 342v-86l-342 214-342-214v86l342 212zM854 170q34 0 59 26t25 60v512q0 34-25 60t-59 26h-684q-34 0-59-26t-25-60v-512q0-34 25-60t59-26h684z"
-        ></path></svg
-      ><svg viewBox="0 0 877.7142857142857 1024" class="home-icon02">
-        <path
-          d="M135.429 808h132v-396.571h-132v396.571zM276 289.143c-0.571-38.857-28.571-68.571-73.714-68.571s-74.857 29.714-74.857 68.571c0 37.714 28.571 68.571 73.143 68.571h0.571c46.286 0 74.857-30.857 74.857-68.571zM610.286 808h132v-227.429c0-121.714-65.143-178.286-152-178.286-70.857 0-102.286 39.429-119.429 66.857h1.143v-57.714h-132s1.714 37.143 0 396.571v0h132v-221.714c0-11.429 0.571-23.429 4-32 9.714-23.429 31.429-48 68-48 47.429 0 66.286 36 66.286 89.714v212zM877.714 237.714v548.571c0 90.857-73.714 164.571-164.571 164.571h-548.571c-90.857 0-164.571-73.714-164.571-164.571v-548.571c0-90.857 73.714-164.571 164.571-164.571h548.571c90.857 0 164.571 73.714 164.571 164.571z"
-        ></path></svg
-      ><svg viewBox="0 0 877.7142857142857 1024" class="home-icon04">
-        <path
-          d="M438.857 73.143c242.286 0 438.857 196.571 438.857 438.857 0 193.714-125.714 358.286-300 416.571-22.286 4-30.286-9.714-30.286-21.143 0-14.286 0.571-61.714 0.571-120.571 0-41.143-13.714-67.429-29.714-81.143 97.714-10.857 200.571-48 200.571-216.571 0-48-17.143-86.857-45.143-117.714 4.571-11.429 19.429-56-4.571-116.571-36.571-11.429-120.571 45.143-120.571 45.143-34.857-9.714-72.571-14.857-109.714-14.857s-74.857 5.143-109.714 14.857c0 0-84-56.571-120.571-45.143-24 60.571-9.143 105.143-4.571 116.571-28 30.857-45.143 69.714-45.143 117.714 0 168 102.286 205.714 200 216.571-12.571 11.429-24 30.857-28 58.857-25.143 11.429-89.143 30.857-127.429-36.571-24-41.714-67.429-45.143-67.429-45.143-42.857-0.571-2.857 26.857-2.857 26.857 28.571 13.143 48.571 64 48.571 64 25.714 78.286 148 52 148 52 0 36.571 0.571 70.857 0.571 81.714 0 11.429-8 25.143-30.286 21.143-174.286-58.286-300-222.857-300-416.571 0-242.286 196.571-438.857 438.857-438.857zM166.286 703.429c1.143-2.286-0.571-5.143-4-6.857-3.429-1.143-6.286-0.571-7.429 1.143-1.143 2.286 0.571 5.143 4 6.857 2.857 1.714 6.286 1.143 7.429-1.143zM184 722.857c2.286-1.714 1.714-5.714-1.143-9.143-2.857-2.857-6.857-4-9.143-1.714-2.286 1.714-1.714 5.714 1.143 9.143 2.857 2.857 6.857 4 9.143 1.714zM201.143 748.571c2.857-2.286 2.857-6.857 0-10.857-2.286-4-6.857-5.714-9.714-3.429-2.857 1.714-2.857 6.286 0 10.286s7.429 5.714 9.714 4zM225.143 772.571c2.286-2.286 1.143-7.429-2.286-10.857-4-4-9.143-4.571-11.429-1.714-2.857 2.286-1.714 7.429 2.286 10.857 4 4 9.143 4.571 11.429 1.714zM257.714 786.857c1.143-3.429-2.286-7.429-7.429-9.143-4.571-1.143-9.714 0.571-10.857 4s2.286 7.429 7.429 8.571c4.571 1.714 9.714 0 10.857-3.429zM293.714 789.714c0-4-4.571-6.857-9.714-6.286-5.143 0-9.143 2.857-9.143 6.286 0 4 4 6.857 9.714 6.286 5.143 0 9.143-2.857 9.143-6.286zM326.857 784c-0.571-3.429-5.143-5.714-10.286-5.143-5.143 1.143-8.571 4.571-8 8.571 0.571 3.429 5.143 5.714 10.286 4.571s8.571-4.571 8-8z"
-        ></path>
-      </svg>
+      <a href="mailto:Joshbarr52@gmail.com?subject=" class="home-link">
+        <svg viewBox="0 0 1024 1024" class="home-icon">
+          <path
+            d="M854 342v-86l-342 214-342-214v86l342 212zM854 170q34 0 59 26t25 60v512q0 34-25 60t-59 26h-684q-34 0-59-26t-25-60v-512q0-34 25-60t59-26h684z"
+          ></path>
+        </svg>
+      </a>
+      <a
+        href="https://www.linkedin.com/in/josh-barr-cs/"
+        target="_blank"
+        rel="noreferrer noopener"
+        class="home-link01"
+      >
+        <svg viewBox="0 0 877.7142857142857 1024" class="home-icon02">
+          <path
+            d="M135.429 808h132v-396.571h-132v396.571zM276 289.143c-0.571-38.857-28.571-68.571-73.714-68.571s-74.857 29.714-74.857 68.571c0 37.714 28.571 68.571 73.143 68.571h0.571c46.286 0 74.857-30.857 74.857-68.571zM610.286 808h132v-227.429c0-121.714-65.143-178.286-152-178.286-70.857 0-102.286 39.429-119.429 66.857h1.143v-57.714h-132s1.714 37.143 0 396.571v0h132v-221.714c0-11.429 0.571-23.429 4-32 9.714-23.429 31.429-48 68-48 47.429 0 66.286 36 66.286 89.714v212zM877.714 237.714v548.571c0 90.857-73.714 164.571-164.571 164.571h-548.571c-90.857 0-164.571-73.714-164.571-164.571v-548.571c0-90.857 73.714-164.571 164.571-164.571h548.571c90.857 0 164.571 73.714 164.571 164.571z"
+          ></path>
+        </svg>
+      </a>
+      <a
+        href="https://github.com/Joshua-Barr"
+        target="_blank"
+        rel="noreferrer noopener"
+        class="home-link02"
+      >
+        <svg viewBox="0 0 877.7142857142857 1024" class="home-icon04">
+          <path
+            d="M438.857 73.143c242.286 0 438.857 196.571 438.857 438.857 0 193.714-125.714 358.286-300 416.571-22.286 4-30.286-9.714-30.286-21.143 0-14.286 0.571-61.714 0.571-120.571 0-41.143-13.714-67.429-29.714-81.143 97.714-10.857 200.571-48 200.571-216.571 0-48-17.143-86.857-45.143-117.714 4.571-11.429 19.429-56-4.571-116.571-36.571-11.429-120.571 45.143-120.571 45.143-34.857-9.714-72.571-14.857-109.714-14.857s-74.857 5.143-109.714 14.857c0 0-84-56.571-120.571-45.143-24 60.571-9.143 105.143-4.571 116.571-28 30.857-45.143 69.714-45.143 117.714 0 168 102.286 205.714 200 216.571-12.571 11.429-24 30.857-28 58.857-25.143 11.429-89.143 30.857-127.429-36.571-24-41.714-67.429-45.143-67.429-45.143-42.857-0.571-2.857 26.857-2.857 26.857 28.571 13.143 48.571 64 48.571 64 25.714 78.286 148 52 148 52 0 36.571 0.571 70.857 0.571 81.714 0 11.429-8 25.143-30.286 21.143-174.286-58.286-300-222.857-300-416.571 0-242.286 196.571-438.857 438.857-438.857zM166.286 703.429c1.143-2.286-0.571-5.143-4-6.857-3.429-1.143-6.286-0.571-7.429 1.143-1.143 2.286 0.571 5.143 4 6.857 2.857 1.714 6.286 1.143 7.429-1.143zM184 722.857c2.286-1.714 1.714-5.714-1.143-9.143-2.857-2.857-6.857-4-9.143-1.714-2.286 1.714-1.714 5.714 1.143 9.143 2.857 2.857 6.857 4 9.143 1.714zM201.143 748.571c2.857-2.286 2.857-6.857 0-10.857-2.286-4-6.857-5.714-9.714-3.429-2.857 1.714-2.857 6.286 0 10.286s7.429 5.714 9.714 4zM225.143 772.571c2.286-2.286 1.143-7.429-2.286-10.857-4-4-9.143-4.571-11.429-1.714-2.857 2.286-1.714 7.429 2.286 10.857 4 4 9.143 4.571 11.429 1.714zM257.714 786.857c1.143-3.429-2.286-7.429-7.429-9.143-4.571-1.143-9.714 0.571-10.857 4s2.286 7.429 7.429 8.571c4.571 1.714 9.714 0 10.857-3.429zM293.714 789.714c0-4-4.571-6.857-9.714-6.286-5.143 0-9.143 2.857-9.143 6.286 0 4 4 6.857 9.714 6.286 5.143 0 9.143-2.857 9.143-6.286zM326.857 784c-0.571-3.429-5.143-5.714-10.286-5.143-5.143 1.143-8.571 4.571-8 8.571 0.571 3.429 5.143 5.714 10.286 4.571s8.571-4.571 8-8z"
+          ></path>
+        </svg>
+      </a>
       <div class="home-links-container">
-        <a href="#Experience" class="home-link Navbar-Link">Experience</a>
-        <a href="#Projects" class="home-link01">
+        <a href="#Experience" class="home-link03 Navbar-Link">Experience</a>
+        <a href="#Projects" class="home-link04">
           <span class="Navbar-Link">Projects</span>
           <br />
         </a>
-        <span class="home-link02">
+        <a
+          href="https://docs.google.com/document/d/e/2PACX-1vRUJMNHwKgAq7guCpVPNNT0-ZK-QFRHmQT3XGJUxCDVZaAV9xNs3zU6pkjgbPaIfWl_7SOpRqtO4Rou/pub"
+          class="home-link05"
+        >
           <span>Download Resume</span>
           <br />
-        </span>
+        </a>
       </div>
       <div data-role="BurgerMenu" class="home-burger-menu">
         <svg viewBox="0 0 1024 1024" class="home-icon06">
@@ -45,9 +66,9 @@
           </div>
         </div>
         <div class="home-links-container1">
-          <span class="home-link03 Navbar-Link">About</span>
-          <span class="home-link04 Navbar-Link">Experience</span>
-          <span class="home-link05 Navbar-Link">Portofolio</span>
+          <span class="home-link06 Navbar-Link">About</span>
+          <span class="home-link07 Navbar-Link">Experience</span>
+          <span class="home-link08 Navbar-Link">Portofolio</span>
           <span class="Navbar-Link">Contact</span>
         </div>
       </div>
@@ -65,7 +86,7 @@
             ever-changing threat landscape using the latest tools and
             technologies. As a Cyber Security Specialist, it&apos;s also my
             passion to continue growing and to keep the work fun.
-            <span v-html="rawsp8g"></span>
+            <span v-html="raws52t"></span>
           </span>
         </span>
         <div class="home-cta-btn-container">
@@ -73,22 +94,6 @@
             <span class="home-text10">Get in Contact</span>
           </a>
         </div>
-      </div>
-    </div>
-    <div class="home-medium">
-      <strong class="home-heading3">Check out Medium</strong>
-      <div class="home-div">
-        <DangerousHTML
-          html="<div id='retainable-rss-embed' data-rss='https://medium.com/feed/retainable,https://medium.com/feed/vue-mastery'
-  data-maxcols='3' 
-  data-layout='grid' 
-  data-poststyle='inline' 
-  data-readmore='Read the rest' 
-  data-buttonclass='btn btn-primary' 
-  data-offset='-100'>
-  
-  <script src='https://www.twilik.com/assets/retainable/rss-embed/retainable-rss-embed.js'></script>"
-        ></DangerousHTML>
       </div>
     </div>
     <div class="home-projects">
@@ -381,16 +386,13 @@
           <div class="home-contact">
             <span class="home-text67">
               <span class="home-text68">Email:</span>
-              <span><span v-html="rawtioe"></span></span>
+              <span><span v-html="rawj8xb"></span></span>
               <a href="mailto:Joshbarr52@gmail.com?subject=Nice to e-meet you :)">
                 Joshbarr52@gmail.com
               </a>
               <br />
               <span class="home-text71">Medium:</span>
-              <span>
-                site
-                <span v-html="rawtjaa"></span>
-              </span>
+              <span> </span>
               <br />
               <span class="home-text74">Linkedin:</span>
               <span> </span>
@@ -398,55 +400,34 @@
                 href="https://www.linkedin.com/in/josh-barr-cs/"
                 target="_blank"
                 rel="noreferrer noopener"
-                class="home-link14"
+                class="home-link17"
               >
                 https://www.linkedin.com/in/josh-barr-cs/
               </a>
               <br />
             </span>
           </div>
-          <div class="home-socials">
-            <div class="home-icon-group">
-              <svg viewBox="0 0 950.8571428571428 1024" class="home-icon10">
-                <path
-                  d="M925.714 233.143c-25.143 36.571-56.571 69.143-92.571 95.429 0.571 8 0.571 16 0.571 24 0 244-185.714 525.143-525.143 525.143-104.571 0-201.714-30.286-283.429-82.857 14.857 1.714 29.143 2.286 44.571 2.286 86.286 0 165.714-29.143 229.143-78.857-81.143-1.714-149.143-54.857-172.571-128 11.429 1.714 22.857 2.857 34.857 2.857 16.571 0 33.143-2.286 48.571-6.286-84.571-17.143-148-91.429-148-181.143v-2.286c24.571 13.714 53.143 22.286 83.429 23.429-49.714-33.143-82.286-89.714-82.286-153.714 0-34.286 9.143-65.714 25.143-93.143 90.857 112 227.429 185.143 380.571 193.143-2.857-13.714-4.571-28-4.571-42.286 0-101.714 82.286-184.571 184.571-184.571 53.143 0 101.143 22.286 134.857 58.286 41.714-8 81.714-23.429 117.143-44.571-13.714 42.857-42.857 78.857-81.143 101.714 37.143-4 73.143-14.286 106.286-28.571z"
-                ></path></svg
-              ><svg viewBox="0 0 877.7142857142857 1024" class="home-icon12">
-                <path
-                  d="M585.143 512c0-80.571-65.714-146.286-146.286-146.286s-146.286 65.714-146.286 146.286 65.714 146.286 146.286 146.286 146.286-65.714 146.286-146.286zM664 512c0 124.571-100.571 225.143-225.143 225.143s-225.143-100.571-225.143-225.143 100.571-225.143 225.143-225.143 225.143 100.571 225.143 225.143zM725.714 277.714c0 29.143-23.429 52.571-52.571 52.571s-52.571-23.429-52.571-52.571 23.429-52.571 52.571-52.571 52.571 23.429 52.571 52.571zM438.857 152c-64 0-201.143-5.143-258.857 17.714-20 8-34.857 17.714-50.286 33.143s-25.143 30.286-33.143 50.286c-22.857 57.714-17.714 194.857-17.714 258.857s-5.143 201.143 17.714 258.857c8 20 17.714 34.857 33.143 50.286s30.286 25.143 50.286 33.143c57.714 22.857 194.857 17.714 258.857 17.714s201.143 5.143 258.857-17.714c20-8 34.857-17.714 50.286-33.143s25.143-30.286 33.143-50.286c22.857-57.714 17.714-194.857 17.714-258.857s5.143-201.143-17.714-258.857c-8-20-17.714-34.857-33.143-50.286s-30.286-25.143-50.286-33.143c-57.714-22.857-194.857-17.714-258.857-17.714zM877.714 512c0 60.571 0.571 120.571-2.857 181.143-3.429 70.286-19.429 132.571-70.857 184s-113.714 67.429-184 70.857c-60.571 3.429-120.571 2.857-181.143 2.857s-120.571 0.571-181.143-2.857c-70.286-3.429-132.571-19.429-184-70.857s-67.429-113.714-70.857-184c-3.429-60.571-2.857-120.571-2.857-181.143s-0.571-120.571 2.857-181.143c3.429-70.286 19.429-132.571 70.857-184s113.714-67.429 184-70.857c60.571-3.429 120.571-2.857 181.143-2.857s120.571-0.571 181.143 2.857c70.286 3.429 132.571 19.429 184 70.857s67.429 113.714 70.857 184c3.429 60.571 2.857 120.571 2.857 181.143z"
-                ></path></svg
-              ><svg viewBox="0 0 602.2582857142856 1024" class="home-icon14">
-                <path
-                  d="M548 6.857v150.857h-89.714c-70.286 0-83.429 33.714-83.429 82.286v108h167.429l-22.286 169.143h-145.143v433.714h-174.857v-433.714h-145.714v-169.143h145.714v-124.571c0-144.571 88.571-223.429 217.714-223.429 61.714 0 114.857 4.571 130.286 6.857z"
-                ></path>
-              </svg>
-            </div>
-          </div>
         </div>
       </div>
     </div>
-    <span class="home-link15 Anchor">
+    <span class="home-link18 Anchor">
       Static Site Hosted in Azure, Built by Josh using Vue 2023
     </span>
   </div>
 </template>
 
 <script>
-import DangerousHTML from 'dangerous-html/vue'
-
 import GalleryCard1 from '../components/gallery-card1'
 
 export default {
   name: 'Home',
   components: {
-    DangerousHTML,
     GalleryCard1,
   },
   data() {
     return {
-      rawsp8g: ' ',
-      rawtioe: ' ',
-      rawtjaa: ' ',
+      raws52t: ' ',
+      rawj8xb: ' ',
     }
   },
   metaInfo: {
@@ -497,24 +478,36 @@ export default {
   text-transform: none;
   text-decoration: none;
 }
+.home-link {
+  display: contents;
+}
 .home-icon {
   fill: var(--dl-color-gray-white);
   width: 24px;
   height: 24px;
   margin-left: var(--dl-space-space-fourunits);
   margin-right: var(--dl-space-space-unit);
+  text-decoration: none;
+}
+.home-link01 {
+  display: contents;
 }
 .home-icon02 {
   fill: var(--dl-color-gray-white);
   width: 24px;
   height: 24px;
   margin-right: var(--dl-space-space-unit);
+  text-decoration: none;
+}
+.home-link02 {
+  display: contents;
 }
 .home-icon04 {
   fill: var(--dl-color-gray-white);
   width: 24px;
   height: 24px;
   margin-right: var(--dl-space-space-unit);
+  text-decoration: none;
 }
 .home-links-container {
   display: flex;
@@ -522,19 +515,19 @@ export default {
   align-items: flex-start;
   flex-direction: row;
 }
-.home-link {
+.home-link03 {
   color: var(--dl-color-gray-white);
   align-self: center;
   margin-right: var(--dl-space-space-tripleunit);
   text-decoration: none;
 }
-.home-link01 {
+.home-link04 {
   color: var(--dl-color-gray-white);
   align-self: center;
   margin-right: var(--dl-space-space-tripleunit);
   text-decoration: none;
 }
-.home-link02 {
+.home-link05 {
   color: var(--dl-color-light-colour1);
   padding: 8px;
   font-size: 24px;
@@ -592,13 +585,13 @@ export default {
   align-items: flex-start;
   flex-direction: column;
 }
-.home-link03 {
+.home-link06 {
   margin-bottom: var(--dl-space-space-unit);
 }
-.home-link04 {
+.home-link07 {
   margin-bottom: var(--dl-space-space-unit);
 }
-.home-link05 {
+.home-link08 {
   margin-bottom: var(--dl-space-space-unit);
 }
 .home-whoami {
@@ -659,33 +652,6 @@ export default {
 }
 .home-text10 {
   color: var(--dl-color-gray-white);
-}
-.home-medium {
-  width: 100%;
-  height: 380px;
-  display: flex;
-  position: relative;
-  max-width: var(--dl-size-size-maxcontent);
-  align-items: center;
-  padding-top: var(--dl-space-space-doubleunit);
-  padding-left: var(--dl-space-space-tripleunit);
-  padding-right: var(--dl-space-space-tripleunit);
-  flex-direction: column;
-  justify-content: center;
-  background-color: var(--dl-color-light-background);
-}
-.home-heading3 {
-  color: var(--dl-color-gray-white);
-  font-size: 34px;
-  margin-top: var(--dl-space-space-doubleunit);
-  font-family: Nunito;
-  margin-bottom: var(--dl-space-space-doubleunit);
-}
-.home-div {
-  width: 100%;
-  height: 245px;
-  align-self: center;
-  margin-bottom: var(--dl-space-space-fiveunits);
 }
 .home-projects {
   width: 1120px;
@@ -1463,36 +1429,10 @@ export default {
 .home-text74 {
   font-weight: 700;
 }
-.home-link14 {
+.home-link17 {
   text-decoration: underline;
 }
-.home-socials {
-  display: flex;
-  align-items: flex-start;
-  flex-direction: column;
-  justify-content: flex-start;
-}
-.home-icon-group {
-  display: flex;
-  align-items: center;
-  flex-direction: row;
-  justify-content: flex-start;
-}
-.home-icon10 {
-  width: 24px;
-  height: 24px;
-  margin-right: var(--dl-space-space-unit);
-}
-.home-icon12 {
-  width: 24px;
-  height: 24px;
-  margin-right: var(--dl-space-space-unit);
-}
-.home-icon14 {
-  width: 24px;
-  height: 24px;
-}
-.home-link15 {
+.home-link18 {
   color: var(--dl-color-gray-white);
   align-self: center;
   margin-top: var(--dl-space-space-fiveunits);
@@ -1523,15 +1463,6 @@ export default {
   .home-text04 {
     align-self: center;
     text-align: left;
-  }
-  .home-medium {
-    height: auto;
-    align-items: center;
-    flex-direction: column;
-    justify-content: center;
-  }
-  .home-heading3 {
-    text-align: center;
   }
   .home-projects {
     width: 98%;
